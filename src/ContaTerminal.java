5e1c3d7dfb1f9282e47ac59640bf28231834f457
italo_costa <italo.costa13@hotmail.com>
2024-05-17 19:01:04 -0300

    Criando novo projeto de simulação de conta bancária

---
+++ src/ContaTerminal.java
@@ -0,0 +1,31 @@
+/**
+ * Este projeto tem como finalidade entregar o desafio proposto pela DIO, no
+ * módulo de Java básico.
+ * Um simulador de conta bancária onde serão solicitados os dados do usuário
+ * [agencia, número da conta, nome e saldo].
+ * Em seguida, será exibida uma mensagem no terminal mostrando todas as
+ * informações fornecidas pelo usuário
+ */
+
+/*
+ * Exigências:
+ * 
+ * Conta - Int ; Agencia - String; Nome - String; Saldo - Double;
+ * Usar a classe Scanner para entrada de dados
+ * Mensagens antes da entrada dos dados
+ * 
+ * Ao finalizar:
+ * 
+ * "Olá [nome], obrigado por criar uma conta em nosso banco,
+ * sua agencia é [Agencia], conta [número da conta],
+ * e seu saldo [saldo] já está disponível para saque!"
+ * 
+ */
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        System.out.println("Teste 1");
+    }
+
+}
